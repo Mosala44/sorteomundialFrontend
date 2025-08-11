aa4303c2ff5761ebed18045370d639870f2dd589
Mosala44 <javiervarasbolados@gmail.com>
2025-08-10 23:43:48 -0400

    grupos sorteados

--- src/grupos.jsx
+++ src/grupos.jsx
@@ -4,26 +4,40 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { useEffect, useState } from 'react';
 import './index.css'
 
 function Grupos() {
+  const [grupos, setGrupos] = useState([]);
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/grupos/obtener/4')
+      .then(response => response.json())
+      .then(data => setGrupos(data.grupos))
+      .catch(error => console.error('Error fetching groups:', error));
+  }, []);
+
   return (
     <div>
-      <div className="alert alert-danger display-1 text-center"><h2><strong>FASE DE GRUPOS</strong></h2></div>
+      <div className="alert alert-danger display-1 text-center">
+        <h2><strong>FASE DE GRUPOS</strong></h2>
+      </div>
       <div className="container mt-5">
         <div className="row">
-            <div className="col-4">
-                <div class="card">
-                  <div class="card-header">
-                    Grupo A
-                  </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                  </ul>
+          {grupos && grupos.map((grupo, index) => (
+            <div key={index} className="col-md-3 mb-4"> {/* 4 columnas por fila en md (>=768px) */}
+              <div className="card h-100"> {/* h-100 para que todas las cards tengan igual altura */}
+                <div className="card-header">
+                  Grupo {String.fromCharCode(65 + index)}
                 </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">{grupo.bombo1.nombre}</li>
+                  <li className="list-group-item">{grupo.bombo2.nombre}</li>
+                  <li className="list-group-item">{grupo.bombo3.nombre}</li>
+                  <li className="list-group-item">{grupo.bombo4.nombre}</li>
+                </ul>
+              </div>
             </div>
+          ))}
         </div>
       </div>
       <footer className="text-center mt-5 text-muted">
@@ -33,5 +47,6 @@ function Grupos() {
     </div>
   );
 }
+
 
 export default Grupos;
